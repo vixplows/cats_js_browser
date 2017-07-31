e9ef208023e1052ac033dab1db30337cb32fc21b
vixplows <vicky@vickyplows.co.uk>
2017-07-31 20:43:24 +0100

    Can add cats via catArray in javascript

--- public/app.js
+++ public/app.js
@@ -26,7 +26,7 @@ var createFaveFood = function(food) {
 };
 
 var createImage = function(imageUrl) {
-  var li3 = document.createElement('li');
+  var li3 = document.createElement('li.no-bullet');
   var img = document.createElement('img');
   img.style.width = "500px";
   img.src = imageUrl;
@@ -43,9 +43,35 @@ var appendElements = function(ul, li1, li2, li3) {
   cats.appendChild(ul);
 };
 
+var catArray = [
+  { name: "Boba",
+    food: "Sock fluff",
+    imageUrl:
+    "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg" },
+  { name: "Barnaby",
+    food: "Tuna",
+    imageUrl: "https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg"
+   },
+   { name: "Max",
+     food: "Whiskas Temptations",
+     imageUrl: " http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg"
+   },
+   { name: "Bodger",
+     food: "Mice",
+     imageUrl: "http://www.catadoption.co.uk/wp-content/uploads/2013/07/ea4c22547edfcf6b06c5c0bec38e3800.jpg" },
+];
+
+var addCatArray = function(catArray) {
+  for (cat of catArray) {
+    addCat(cat.name, cat.food, cat.imageUrl);
+  };
+}
+
 var app = function(){
- addCat("Bodger", "Mice", "http://www.catadoption.co.uk/wp-content/uploads/2013/07/ea4c22547edfcf6b06c5c0bec38e3800.jpg");
+  addCatArray(catArray);
+ // addCat("Bodger", "Mice", "http://www.catadoption.co.uk/wp-content/uploads/2013/07/ea4c22547edfcf6b06c5c0bec38e3800.jpg");
 };
+
 
   window.addEventListener('load', app);
 
