e139904fc298137d805113d145c09a6d7cd7642b
vixplows <vicky@vickyplows.co.uk>
2017-07-31 20:46:18 +0100

    Remove comments

--- public/app.js
+++ public/app.js
@@ -69,33 +69,7 @@ var addCatArray = function(catArray) {
 
 var app = function(){
   addCatArray(catArray);
- // addCat("Bodger", "Mice", "http://www.catadoption.co.uk/wp-content/uploads/2013/07/ea4c22547edfcf6b06c5c0bec38e3800.jpg");
 };
 
 
   window.addEventListener('load', app);
-
-
-  // var ul = document.createElement('ul');
-  // ul.classList.add('cat');
-
-  // var li1 = document.createElement('li');
-  // li1.innerText = "Name: Sophie";
-
-  // var li2 = document.createElement('li');
-  // li2.innerText = "Favourite food: Chicken";
-  
-  // var li3 = document.createElement('li');
-  // var image = document.createElement('img');
-  // image.style.width = "500px";
-  // image.src = "https://s-media-cache-ak0.pinimg.com/736x/62/1b/98/621b98767ec0c7aadaacf87ed0f1cc69.jpg";
-
-// // need to add ensure the added image is also linked to class="no-bullet"
-
-  // li3.appendChild(image);
-  // ul.appendChild(li1);
-  // ul.appendChild(li2);
-  // ul.appendChild(li3);
-
-//   var cats = document.querySelector('#cats');
-//   cats.appendChild(ul);
